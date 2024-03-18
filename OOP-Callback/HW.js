3d20a03cff33d43011d83e998f61373171ff4f69
AnaSarivan <115470465+AnaSarivan@users.noreply.github.com>
2024-03-18 11:45:09 +0200

    Update HW.js

--- OOP-Callback/HW.js
+++ OOP-Callback/HW.js
@@ -2,21 +2,19 @@
 class NumberContainer {
     // HW1: try to set initial values through 
     //constructor - new NumberContainer(2, 4, 6)
-    constructor(v_0, v_1, v_2, maxLen = 3) {
+    constructor(v_0, v_1, v_2) {
         this.val_0 = v_0
         this.val_1 = v_1
         this.val_2 = v_2
 
-        this.maxLen = maxLen
-
     }
 
     get length () {
-        for (let i=0; i<this.maxLen; i++) {
+        for (let i=0; i<3; i++) {
             if (this[`val_${i}`] == undefined) {
                 return i
             }
         }
-        return this.maxLen
+        return 3
     }
 }
